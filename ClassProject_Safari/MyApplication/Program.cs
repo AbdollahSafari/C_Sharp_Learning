1e6b550ea26bcc94c61bb492253fce15b4490121
AbdollahSafari <abdollahsafari1986@gmail.com>
2023-08-16 16:55:40 +0330

    Start Project With Login Form

--- ClassProject_Safari/MyApplication/Program.cs
+++ ClassProject_Safari/MyApplication/Program.cs
@@ -16,7 +16,7 @@ internal static class Program
 		// **************************************************
 
 		// **************************************************
-		var mainForm = new StartupForm();
+		var mainForm = new LoginForm();
 
 		System.Windows.Forms.Application.Run(mainForm: mainForm);
 
