73a8ae68e50af209de60a7541f6b8e2ad954f5f5
AbdollahSafari <abdollahsafari1986@gmail.com>
2023-09-04 19:57:32 +0330

    newupdate

---
+++ ClassProject_Safari/MyApplication/RegisterForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace MyApplication
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Dtat.Windows.Forms.Label();
+            textBox1 = new Dtat.Windows.Forms.TextBox();
+            label2 = new Dtat.Windows.Forms.Label();
+            textBox2 = new Dtat.Windows.Forms.TextBox();
+            label3 = new Dtat.Windows.Forms.Label();
+            textBox3 = new Dtat.Windows.Forms.TextBox();
+            button1 = new Dtat.Windows.Forms.Button();
+            button2 = new Dtat.Windows.Forms.Button();
+            button3 = new Dtat.Windows.Forms.Button();
+            button4 = new Dtat.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(50, 20);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(68, 6);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(394, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(12, 42);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(50, 20);
+            label2.TabIndex = 2;
+            label2.Text = "label2";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(68, 39);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(394, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(12, 75);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(50, 20);
+            label3.TabIndex = 4;
+            label3.Text = "label3";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new System.Drawing.Point(68, 72);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(394, 27);
+            textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(68, 105);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(94, 29);
+            button1.TabIndex = 6;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(168, 105);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(94, 29);
+            button2.TabIndex = 7;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(268, 105);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(94, 29);
+            button3.TabIndex = 8;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new System.Drawing.Point(368, 105);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(94, 29);
+            button4.TabIndex = 9;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // RegisterForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(468, 141);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "RegisterForm";
+            Text = "RegisterForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Dtat.Windows.Forms.Label label1;
+        private Dtat.Windows.Forms.TextBox textBox1;
+        private Dtat.Windows.Forms.Label label2;
+        private Dtat.Windows.Forms.TextBox textBox2;
+        private Dtat.Windows.Forms.Label label3;
+        private Dtat.Windows.Forms.TextBox textBox3;
+        private Dtat.Windows.Forms.Button button1;
+        private Dtat.Windows.Forms.Button button2;
+        private Dtat.Windows.Forms.Button button3;
+        private Dtat.Windows.Forms.Button button4;
+    }
+}
